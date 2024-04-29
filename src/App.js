49541f8ad1c19ab27b88d36a955ffe713ae87f11
sarthakdhasmana <Sarthakmay09@gmail.com>
2024-04-29 13:06:08 +0530

    frontend task file copied for website launch'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HeroSection from './Components/HeroSection';
+import AstrixSection from './Components/AstrixSection'
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+      <AstrixSection />
+    </div>
+  );
+}
+
+export default App;
